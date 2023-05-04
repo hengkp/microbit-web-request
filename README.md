a5367268a2b5c7a2920fb02eb20d6269206d7a7b
hengkp <40425349+hengkp@users.noreply.github.com>
2023-05-04 11:27:53 +0700

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
+
 # microbit-web-request
 A MakeCode extension for sending and receiving HTTP requests.
+
+> Open this page at [https://github.com/hengkp/microbit-web-request](https://github.com/hengkp/microbit-web-request)
+
+## Use as an Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open []()
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **github:hengkp/microbit-web-request** and import
+
+## Edit this project ![Build status badge](https://github.com/hengkp/microbit-web-request/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open []()
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/hengkp/microbit-web-request** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/hengkp/microbit-web-request/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ libs/webRequest.ts
@@ -0,0 +1,81 @@
+/**
+ * MakeCode extension for sending and receiving HTTP requests.
+ */
+ namespace webRequest {
+    declare var XMLHttpRequest: new () => XMLHttpRequest;
+
+    /**
+     * Performs an HTTP GET request and returns the response text.
+     * @param url The URL to send the request to.
+     * @param headers Optional headers to include in the request.
+     */
+    //% block="httpGet url=$url || headers=$headers"
+    export function httpGet(url: string, headers?: any): string {
+        let request = new XMLHttpRequest();
+        request.open("GET", url, false);
+        if (headers) {
+            for (let headerName in headers) {
+                request.setRequestHeader(headerName, headers[headerName]);
+            }
+        }
+        request.send(null);
+        if (request.status == 200) {
+            return request.responseText;
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Performs an HTTP POST request and returns the response text.
+     * @param url The URL to send the request to.
+     * @param data The data to include in the request body.
+     * @param headers Optional headers to include in the request.
+     */
+    //% block="httpPost url=$url || data=$data || headers=$headers"
+    export function httpPost(url: string, data: string, headers?: any): string {
+        let request = new XMLHttpRequest();
+        request.open("POST", url, false);
+        if (headers) {
+            for (let headerName in headers) {
+                request.setRequestHeader(headerName, headers[headerName]);
+            }
+        }
+        request.send(data);
+        if (request.status == 200) {
+            return request.responseText;
+        } else {
+            return "";
+        }
+    }
+
+    /**
+     * Converts multiple MakeCode variables to a JSON object.
+     * @param vars An object containing the variable names and values.
+     */
+    //% block="toJsonData with $vars"
+    //% inlineInputMode=inline
+    //% expandableArgumentMode="enabled"
+    export function toJsonData(...vars: any[]): any {
+        const jsonData: {[key: string]: any} = {};
+        for (let i = 0; i < vars.length; i++) {
+            let varName = Object.keys(vars[i])[0];
+            let varValue = vars[i][varName];
+            jsonData[varName] = varValue;
+        }
+        return jsonData;
+    }
+
+    /**
+     * Create an object from variable name and value.
+     * @param varName The name of the variable.
+     * @param varValue The value of the variable.
+     * @returns An object with the variable name and value.
+     */
+    //% block="createObject with name $varName value $varValue"
+    export function createObject(varName: string, varValue: number): { [key: string]: number } {
+        const obj: { [key: string]: number } = {};
+        obj[varName] = varValue;
+        return obj;
+    }
+}
